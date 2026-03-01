67cd21e97d7401aa6056f2ee96fd2124a57e7661
NotKeKe <149957866+NotKeKe@users.noreply.github.com>
2026-03-01 20:49:20 +0800

    修復某些 x 頁面會出現的 UserProtected error
    deepwiki 透過複寫 Community 的內部方法而成

--- src/services/twitter/utils.py
+++ src/services/twitter/utils.py
@@ -1,9 +1,11 @@
 from tweety import TwitterAsync
-from tweety.types import Media, Tweet 
+from tweety.types import Media, Tweet, Community, User
+from tweety.exceptions import UserProtected
 from typing import cast, Optional, Any
 import logging
 from pathlib import Path
 from urllib.parse import urlparse
+
 
 from src.config import COOKIES, DATA_DIR, BASE_DIR, DOWNLOAD_DIR
 from src.sqlite import insert_tweet_media, is_tweet_media_exists, insert_passed_tweet
@@ -16,10 +18,43 @@ client: TwitterAsync | None = None
 session_name = str((DATA_DIR / "twitter_session").relative_to(BASE_DIR))
 session_path = Path(session_name + 'tw_session')
 
+# deepwiki did this, to ignore UserProtected exception
+class SafeUserDataTwitter(TwitterAsync):  
+    def __init__(self, *args, **kwargs):  
+        super().__init__(*args, **kwargs)  
+        self._patch_community_methods()  
+      
+    def _patch_community_methods(self):  
+        """修改 Community 類，實現嘗試抓取邏輯"""  
+          
+        def safe_get_admin(self):  
+            try:  
+                admin_data = self._community.get('admin_results')  
+                if admin_data:  
+                    return User(self._client, admin_data)  
+                return None  
+            except UserProtected:  
+                # 用戶被暫停或其他保護狀態，返回 None  
+                return None  
+          
+        def safe_get_creator(self):  
+            try:  
+                creator_data = self._community.get('creator_results')  
+                if creator_data:  
+                    return User(self._client, creator_data)  
+                return None  
+            except UserProtected:  
+                # 用戶被暫停或其他保護狀態，返回 None  
+                return None  
+          
+        # 應用修改  
+        Community._get_admin = safe_get_admin  
+        Community._get_creator = safe_get_creator  
+
 async def _get_client():
     global client
     if not client:  
-        client = TwitterAsync(session_name)  
+        client = SafeUserDataTwitter(session_name)  
           
         # 先嘗試載入現有會話  
         if client.session.logged_in:  
